248fd16e3c829d422fa418f3ae347b3fa6642466
Louis Ouellet <louis_ouellet@hotmail.com>
2024-11-29 13:40:41 -0500

    Code Cleanup

--- src/configurator.py
+++ src/configurator.py
@@ -926,7 +926,6 @@ class MySQLConfigurator(QMainWindow):
         # Create the checkbox
         check_box = QCheckBox()
         check_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        # check_box.setStyleSheet("padding: 10px; border-radius: 5px; border: 1px solid #76797C;")
 
         # Enable checkbox if value is true/uncommented
         if value.lower() in ["1", "true", "yes", "on", ""]:
